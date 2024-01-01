24b176d6a9580c45c8cf37c660241875d333f32a
arpitghura <ghurarpit110@gmail.com>
2024-01-01 15:33:23 +0530

    made navigation menu responsive

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -2,21 +2,24 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Logo from "../../public/logo.png";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-    const handleMenuClick = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="fixed h-20 w-full shadow-md bg-white">
-      <div className="flex h-full items-center justify-between px-4">
+      <div
+        className={`flex h-full items-center justify-between px-4 ${
+          isMenuOpen ? "delay-200 blur-sm" : "blur-0"
+        }`}
+      >
         <Link href="/">
           <Image
             src={Logo}
@@ -47,7 +50,7 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="hidden md:flex flex-row items-center h-full">
-          <button className="py-3 px-6 rounded-md bg-[#E0E0E3] border">
+          <button className="py-3 px-6 rounded-md bg-secondary border hover:bg-primary hover:text-secondary ease-in duration-300">
             Login
           </button>
           <div className="px-1">
@@ -61,17 +64,67 @@ const Navbar = () => {
               <circle cx="4.5" cy="4.5" r="4.5" fill="#5552B9" />
             </svg>
           </div>
-          <button className="py-3 px-6 rounded-md border border-[#5552B9]">
+          <button className="py-3 px-6 rounded-md border border-primary hover:text-primary hover:bg-secondary ease-in duration-300">
             Signup
           </button>
         </div>
         <div className="flex h-full items-center md:hidden">
-            <button onClick={handleMenuClick}>
-            <HiOutlineMenuAlt3 size={50} className="text-primary"/>
-            </button>
+          <button onClick={handleMenuClick}>
+            <AiOutlineMenu size={50} className="text-primary" />
+          </button>
         </div>
       </div>
-        
+
+      {/* Mobile Menu */}
+      <div
+        className={`bg-secondary rounded-e-xl z-50 ease-in duration-300 p-5 fixed h-screen w-[65%] top-0
+          ${isMenuOpen ? "left-0" : "left-[-100%]"}`}
+      >
+        <div className="flex justify-end">
+          <button onClick={handleMenuClick}>
+            <AiOutlineClose size={35} className="text-gray-700" />
+          </button>
+        </div>
+
+        <div className="flex flex-col h-full w-full py-5">
+          <Link href="#">
+            <p className="py-4 text-primary hover:text-primary">Home</p>
+          </Link>
+          <Link href="#">
+            <p className="py-4 hover:text-primary">Products</p>
+          </Link>
+          <Link href="#">
+            <p className="py-4 hover:text-primary">About Us</p>
+          </Link>
+          <Link href="#">
+            <p className="py-4 hover:text-primary">Blogs</p>
+          </Link>
+          <div className="flex flex-row flex-wrap text-center">
+            <Link
+              href="#"
+              className="mr-2 my-2 py-3 px-6  rounded-md bg border border-black text-primary hover:bg-primary hover:text-secondary ease-in duration-300"
+            >
+              Login
+            </Link>
+            <Link
+              href="#"
+              className="mr-2 my-2 py-3 px-6  rounded-md border border-primary hover:text-primary hover:bg-secondary ease-in duration-300"
+            >
+              Signup
+            </Link>
+          </div>
+
+          <div className="flex items-end h-full w-full">
+            <Image
+              src={Logo}
+              width={150}
+              height={50}
+              alt="Creatvise"
+              className="cursor-pointer user-select-none"
+            />
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
